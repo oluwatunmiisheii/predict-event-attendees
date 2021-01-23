531c5ff92215f751a06c569c50fa5371d680f43c
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-01-23 20:55:22 +0100

    chore: tested out provide function

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import Tix from './tix.js';
+const app = createApp(App)
+app.provide('TixScript', Tix);
+app.provide('location', 'North Pole');
+
+console.log('from main.js', app.provide)
+
 createApp(App).use(store).use(router).mount('#app')
