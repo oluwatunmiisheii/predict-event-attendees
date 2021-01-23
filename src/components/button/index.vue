4bdecb08c9a8bf8fe58b5c37b3d73552f8524e53
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-01-23 20:51:06 +0100

    chore(component): created button component

---
+++ src/components/button/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="trix-button" :class="btnClass" @click="btnClicked">Hello</button>
+</template>
+
+<script>
+export default {
+  props: {
+    btnClass: {
+      type: String,
+      default: 'trix-button__primary',
+      required: false,
+      validator: function(value) {
+        return ['tix-button__primary'].indexOf(value) !== -1
+      }
+    }
+  },
+  
+  setup(props, context) {
+    /**
+     * function to emit a click event when the button component is clicked 
+     */
+    function btnClicked() {
+      context.emit('btnClicked')
+    }
+    return {btnClicked};
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import "@/assets/scss/components/_button.scss";
+</style>
